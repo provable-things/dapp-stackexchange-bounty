b9029bfa1257a201e7fb2831b85b9ff8744c9a3d
Marco Giglio <marcogiglio@gmail.com>
2016-03-14 20:15:55 +0100

    Some updates refactoring

--- NewStackExchangeBounty.sol
+++ NewStackExchangeBounty.sol
@@ -4,14 +4,12 @@ import "dev.oraclize.it/api.sol";
 contract StackExchangeBountyAddress {
     address main;
     uint questionID;
-    uint i;
     string site;
 
     function StackExchangeBountyAddress(uint _questionID, string _site) {
         main = msg.sender;
         questionID = _questionID;
         site = _site;
-        i = _i;
     }
 
     function() {
@@ -28,11 +26,11 @@ contract StackExchangeBounty is usingOraclize {
 
     // solo per debug
     address owner;
-    enum QueryType {Check, Info, Location, Exist}
+    enum QueryType {newQuestion, getWinnerID, getWinnerAddress, isAnswerAccepted}
 
-    struct question {
-        address[] public sponsors;
-        mapping (address => uint) public sponsorsBalance;
+    struct Question {
+        address[] sponsors;
+        mapping (address => uint) sponsorsBalance;
         address contractAddress;
         address winnerAddress;
         uint winnerID;
@@ -40,11 +38,11 @@ contract StackExchangeBounty is usingOraclize {
         uint updateDelay;
         uint expiryDate;
         uint ownedFee;
-        mapping (byte32 => QueryType) queryType;
+        mapping (bytes32 => QueryType) queryType;
     }
 
-    struct site {
-            mapping(uint => question) public questions;
+    struct Site {
+            mapping(uint => Question) questions;
     }
 
     struct QueryInfo {
@@ -52,10 +50,10 @@ contract StackExchangeBounty is usingOraclize {
       uint questionID;
     }
 
-    mapping(byte32 => QueryInfo) queryInfo;
-    mapping(byte32 => site) public sites;
+    mapping(bytes32 => QueryInfo) queryInfo;
+    mapping(bytes32 => Site) public sites;
     uint contractBalance;
-
+    uint totalBounty;
     function StackExchangeBounty() {
 
         // **************** SET NETWORK *************************
@@ -67,55 +65,198 @@ contract StackExchangeBounty is usingOraclize {
     }
 
 
-    function increaseBounty(uint _questionID, string _site, uint i) internal {
-
-
+    function increaseBounty(uint _questionID, string _site) internal {
+        if(msg.value == 0 || sites[_site].questions[_questionID].winnerID != 0) throw;
+        sites[_site].questions[_questionID].sponsors[
+            sites[_site].questions[_questionID].sponsors.length++
+        ] = msg.sender;
+        sites[_site].questions[_questionID].sponsorsBalance[msg.sender] += msg.value;
     }
 
     function handleQuestion(uint _questionID, string _site) {
+        //Check if question exist, if it doesn't add it
+        if(sites[_site].questions[_questionID] == 0) {
+            queryOraclize(
+                0,
+                _questionID,
+                _site,
+                QueryType.newQuestion
+            );
+            sites[_site].questions[_questionID].updateDelay = 600;
+        }
+        increaseBounty(_questionID, _site);
 
     }
 
 
     function __callback(bytes32 queryID, string result) {
         if (msg.sender != oraclize_cbAddress()) throw;
-        _site =  LastQueryInfos[queryID].site;
-        _questionID =  LastQueryInfos[queryID].questionID;
+        uint memory parsedResult = parseInt(result);
+        uint newUpdateDelay = 6000;
+        _site =  queryInfo[queryID].site;
+        _questionID =  queryInfo[queryID].questionID;
 
-        if(sites[_site].questions[_questionID].queryType[queryID] == QueryType.Exist) {
+        if(sites[_site].questions[_questionID].queryType[queryID] == QueryType.newQuestion) {
+            if(bytes(result).length == 0) {
+                resolveContract(_questionID, _site);
+            }
+            else if (parsedResult  > 0) {
+                sites[_site].questions[_questionID].contractAddress =
+                    new StackExchangeBountyAddress(_questionID, _site);
+                queryOraclize(
+                    sites[_site].questions[_questionID].updateDelay,
+                    _questionID,
+                    _site,
+                    QueryType.isAnswerAccepted
+                );
+            }
 
         }
-        else if(sites[_site].questions[_questionID].queryType[queryID] == QueryType.Check) {
+        else if(sites[_site].questions[_questionID].queryType[queryID] == QueryType.isAnswerAccepted) {
+            if(bytes(result).length != 0 && parsedResult  > 0 ) {
+                sites[_site].questions[_questionID].acceptedAnswerID = parsedResult;
+                resolveContract(_questionID, _site);
+            }
+        }
+        else if(sites[_site].questions[_questionID].queryType[queryID] == QueryType.getWinnerID) {
+            if(bytes(result).length == 0) {
+                queryOraclize(
+                    newUpdateDelay,
+                    _questionID,
+                    _site,
+                    QueryType.getWinnerAddress
+                );
+            }
+            else if (parsedResult  > 0) {
+                sites[_site].questions[_questionID].winnerID = parsedResult;
+                resolveContract(_questionID, _site);
+            }
 
         }
-        else if(sites[_site].questions[_questionID].queryType[queryID] == QueryType.Info) {
-
-        }
-        else if(sites[_site].questions[_questionID].queryType[queryID] == QueryType.Location) {
-
+        else if(sites[_site].questions[_questionID].queryType[queryID] == QueryType.getWinnerAddress) {
+            if (bytes(result).length == 0 || bytes(result).length != 42) {
+                queryOraclize(
+                    newUpdateDelay,
+                    _questionID,
+                    _site,
+                    QueryType.getWinnerAddress
+                );
+            }
+            else if (bytes(result).length > 0 && bytes(result).length == 42) {
+                sites[_site].questions[_questionID].winnerAddress = parseAddr(result);
+                resolveContract(_questionID, _site);
+            }
         }
 
     }
 
-    function sendBounty(uint  _questionID, string _site) internal {
+    function resolveContract(uint  _questionID, string _site) internal {
+        if (sites[_site].questions[_questionID].expiryDate < now) throw;
+        if (sites[_site].questions[_questionID].acceptedAnswerID != 0) {
+            if (sites[_site].questions[_questionID].winnerID == 0) {
+                queryOraclize(
+                    0,
+                    _questionID,
+                    _site,
+                    QueryType.getWinnerID
+                );
+            }
+            if (sites[_site].questions[_questionID].winnerAddress == 0) {
+                queryOraclize(
+                    0,
+                    _questionID,
+                    _site,
+                    QueryType.getWinnerAddress
+                );
+            }
 
+            uint memory numSponsors = sites[_site].questions[_questionID].sponsors.length;
+            uint paidFee = sites[_site].questions[_questionID].ownedFee / numSponsors;
+            //Max number of sponsors 255
+            for (uint i = 0; i <= numSponsors; i++) {
+                    sites[_site].questions[_questionID].sponsorsBalance[
+                        sites[_site].questions[_questionID].sponsors[i]
+                    ] -= paidFee;
+                    totalBounty +=
+                    sites[_site].questions[_questionID].sponsorsBalance[
+                        sites[_site].questions[_questionID].sponsors[i]
+                    ];
+            }
+
+            sites[_site].questions[_questionID].winnerAddress.send(bountyAmount);
+
+        }
     }
 
 
-    function queryOraclize(uint _updateDelay, uint _questionID, string _site) internal {
-        string memory URL = strConcat(
-            "https://api.stackexchange.com/2.2/questions/", uint2str(_questionID),
-            "?site=",
-            _site
-          );
+     function queryOraclize(uint _updateDelay, uint _questionID, string _site, QueryType _type) internal {
+
         contractBalance = this.balance;
-        bytes32 queryID = oraclize_query(
-            _updateDelay,
-            URL,
-            strConcat("json(",URL,").items.0.accepted_answer_questionID")
-          );
+        string memory URL;
+        if (_queryType == QueryType.newQuestion) {
+            URL = strConcat(
+                "https://api.stackexchange.com/2.2/questions/",
+                uIntToStr(_questionID),
+                "?site=",
+                _site
+              );
+
+            bytes32 queryID = oraclize_query(
+                "URL",
+                strConcat("json(",URL,").items.0.creation_date")
+              );
+
+        }
+        else if (_queryType == QueryType.getWinnerID) {
+            URL = strConcat(
+                "https://api.stackexchange.com/2.2/answers/",
+                uIntToStr(sites[_site].questions[_questionID].acceptedAnswerID),
+                "?site=",
+                _site
+                );
+
+            bytes32 queryID = oraclize_query(
+                "URL",
+                strConcat("json(",URL,").items.0.owner.user_id")
+              );
+
+
+        }
+        else if (_queryType == QueryType.getWinnerAddress) {
+            string memory URL = strConcat(
+                "https://api.stackexchange.com/2.2/users/",
+                uIntToStr(sites[_site].questions[_questionID].winnerID),
+                "?site=",
+                _site
+                );
+
+            bytes32 queryID = oraclize_query(
+                _updateDelay,
+                "URL",
+                strConcat("json(",URL,").items.0.location")
+              );
+
+        }
+        else if (_queryType == QueryType.isAnswerAccepted) {
+            string memory URL = strConcat(
+                "https://api.stackexchange.com/2.2/questions/", uIntToStr(_questionID),
+                "?site=",
+                _site
+              );
+
+
+
+            bytes32 queryID = oraclize_query(
+                _updateDelay,
+                "URL",
+                strConcat("json(",URL,").items.0.accepted_answer_questionID")
+              );
+
+        }
+
+
         sites[_site].questions[_questionID].ownedFee += (contractBalance - this.balance);
-        sites[_site].questions[_questionID].queryType[queryID] = QueryType.Check;
+        sites[_site].questions[_questionID].queryType[queryID] = _queryType;
         queryInfo[queryID].site = _site;
         queryInfo[queryID].questionID = _questionID;
     }
@@ -126,7 +267,7 @@ contract StackExchangeBounty is usingOraclize {
         if (msg.sender == owner) suicide(msg.sender);
     }
 
-    function uint2str(uint i) returns (string){
+     function uIntToStr(uint i) returns (string){
         uint j = i;
         uint len;
         while (j != 0){
